1e5091e03277f32c9ae80f682337ae52b60c692a
choijun9716 <choijun9716@gmail.com>
2026-05-16 02:15:56 +0900

    Force page scroll to top on refresh

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Force scroll to top on refresh
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0);
+
 document.addEventListener('DOMContentLoaded', () => {
 
 
