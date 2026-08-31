e4334ebdba2396e24d60cbf1c54cddecf660ad29
choijun9716 <choijun9716@gmail.com>
2026-08-31 20:59:09 +0900

    style: 공유 스킴 제목 표기 방식 변경 [브랜드명] 방송일]_라이브 스킴

--- admin_src/src/pages/projects.js
+++ admin_src/src/pages/projects.js
@@ -1116,14 +1116,13 @@ export function renderSharedScheme(params) {
     const brand = store.getById('brands', project.brandId);
     const brandName = project.brandName || (brand ? brand.name : '');
     const broadcastDate = project.broadcastDate || '';
-    const adName = project.adName || '';
-    const titleText = `${brandName ? `[${brandName}] ` : ''}${broadcastDate ? `[${broadcastDate}] ` : ''}${adName}`;
+    const titleText = `[${brandName}] ${broadcastDate}]_라이브 스킴`;
 
     container.innerHTML = `
       <div class="page-header" style="margin-bottom: var(--space-6);">
         <div class="page-header-left">
           <div>
-            <h1 class="page-title" style="font-size: 24px;">${titleText} 라이브 스킴 공유 관리</h1>
+            <h1 class="page-title" style="font-size: 24px;">${titleText}</h1>
             <p class="page-description" style="margin-top: 4px;">브랜드사 공유 전용 기재 페이지입니다. 내용을 작성하고 저장 버튼을 눌러주세요.</p>
           </div>
         </div>
